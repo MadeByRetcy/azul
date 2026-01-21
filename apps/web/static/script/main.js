8abdb2af8ade067e6a9b7333849bc037229cb153
retr0cube <buthelo0@gmail.com>
2026-01-22 00:54:41 +0100

    fix: see #2

--- apps/web/static/script/main.js
+++ apps/web/static/script/main.js
@@ -1,21 +1,6 @@
 {
   // Azul Icon Loader Script
 
-  // TODO: remove this and make a proper CSS file for it
-  const style = document.createElement('style');
-  style.textContent = `
-  span[class*="az-ico:"] {
-      background-color: currentColor; 
-      width: 1em;
-      height: 1em;
-      display: inline-block;
-      -webkit-mask-repeat: no-repeat;
-      mask-repeat: no-repeat;
-      -webkit-mask-size: contain;
-      mask-size: cover;
-  }
-  `;
-  document.head.appendChild(style);
 
   // Regex to match class names starting with "az-ico:" no matter where they are in the class list
   const regex = /\baz-ico:\S+/;
@@ -30,7 +15,9 @@
           if (match) {
               const iconName = match[0].split(':')[1];
               // TODO: Dynamic URL based on environment (Localhost vs Production)
-              const url = `https://azul.msharf.in/registry/fetch?ic=${iconName}`;
+              const url = `${
+                window.location.origin
+              }/registry/fetch?ic=${iconName}`;
               // Using webkitMaskImage for compatibility 
               element.style.webkitMaskImage = `url(${url})`;
               element.style.maskImage = `url(${url})`;
