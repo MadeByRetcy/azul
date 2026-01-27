0d8c85b6e8eb309e5323b735488f043564914854
retr0cube <buthelo0@gmail.com>
2026-01-28 00:41:21 +0100

    feat: added easter egg and ui changes

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
-	import { fade } from 'svelte/transition'
-	import './layout.css'
-	import { navigating, page } from '$app/state'
+	import "./layout.css"
+
+	import { fade } from "svelte/transition"
+
+	import { navigating, page } from "$app/state"
+	import { goto } from "$app/navigation"
 
 	let { children, data } = $props()
+	let { supabase } = $derived(data)
+	
+	let wave: boolean = $state(false)
+
+	setInterval(() => {
+		// There is a 10% chance you'd get a waving hand every 4 seconds, and 1% chance you'd get 2 waves in a row
+		if (Math.random() < 0.1) {
+			wave = true
+			setTimeout(() => (wave = false), 500)
+		}
+	}, 4000)
 </script>
 
 <svelte:head>
@@ -20,10 +34,10 @@
 	<meta property="og:url" content="https://azul.msharf.in" />
 
 	<script defer src="{page.url.origin}/script/main.js"></script>
-	<link rel="stylesheet" href="{page.url.origin}/script/main.css">
+	<link rel="stylesheet" href="{page.url.origin}/script/main.css" />
 </svelte:head>
 
-<header class="relative md:mx-30 md:py-10 mx-8 py-8 p flex items-center justify-between">
+<header class="relative mx-8 flex items-center justify-between py-8 md:mx-30 {page.url.pathname.startsWith("/itri/checkout") ? "md:py-4" : "md:py-10"}">
 	{#if navigating.complete}
 		<span
 			transition:fade={{ duration: 100 }}
@@ -33,48 +47,66 @@
 		</span>
 	{/if}
 	<a href="/">
-		<img src="/icon.webp" alt="Azul Logo" class="md:size-10 size-8" />
+		<img src="/icon.webp" alt="Azul Logo" class="size-8 md:size-10 {wave ? 'waving' : null}" />
 	</a>
-	<div class="flex md:text-lg font-medium">
-		<a href="/registry" class="md:me-8 me-4 flex items-center">
-			<span class="me-1 icon-[charm--layout-dashboard] text-blue-500"></span>
-			<span>Registry</span>
-		</a>
-		<a href="/docs" class="md:me-8 me-4 flex items-center">
-			<span class="me-1 icon-[charm--book-open] text-blue-500"></span>
-			<span class="hidden md:inline">Docs</span>
-		</a>
-		<a href="https://github.com/MadeByRetcy/azul" class="flex items-center md:me-8 me-4">
-			<span class="icon-[charm--github] text-blue-500 me-1"></span>
-			<span class="hidden md:inline">Github</span>
-		</a>
-		<a
-			href={data.session ? "/itri/dashboard" : "/itri"}
-			class="flex items-center rounded-2xl bg-blue-500 px-4 py-2 text-white transition-colors hover:bg-blue-600"
-		>
-			<span class="me-1 icon-[charm--north-star] text-sky-200"></span>
-			<span>
-				{#if data.session}
-					<span class="font-semibold">Itri</span> dashboard
-				{:else}
-					Go <span class="font-semibold">Itri</span>
-				{/if}
-			</span>
-		</a>
-	</div>
+	{#if page.url.pathname.startsWith("/itri/checkout")}
+		<button onclick={
+			async () => {
+				await supabase.auth.signOut()
+				goto("/")
+			}
+		} class="border-orange-500 border py-2 px-4 rounded-xl flex items-center justify-center mb-3 text-orange-500">
+			<span class="me-2 az-ico:riad-leave"></span>
+			<span>Sign out</span>
+		</button>
+	{:else}
+		<div class="flex font-medium md:text-lg">
+			<a href="/registry" class="me-4 flex items-center md:me-8">
+				<span class="me-1 icon-[charm--layout-dashboard] text-blue-500"></span>
+				<span>Registry</span>
+			</a>
+			<a href="/docs" class="flex items-center">
+				<span class="me-1 icon-[charm--book-open] text-blue-500"></span>
+				<span class="hidden md:inline">Docs</span>
+			</a>
+			<!-- <a href="https://github.com/MadeByRetcy/azul" class="me-4 flex items-center md:me-8">
+				<span class="me-1 icon-[charm--github] text-blue-500"></span>
+				<span class="hidden md:inline">Github</span>
+			</a> -->
+			<!-- <a
+				href={data.session ? "/itri/dashboard" : "/itri"}
+				class="flex items-center rounded-2xl bg-blue-500 px-4 py-2 text-white transition-colors hover:bg-blue-600"
+			>
+				<span class="me-1 icon-[charm--north-star] text-sky-200"></span>
+				<span>
+					{#if data.session}
+						<span class="font-semibold">Itri</span> dashboard
+					{:else}
+						Go <span class="font-semibold">Itri</span>
+					{/if}
+				</span>
+			</a> -->
+		</div>
+	{/if}
+
 </header>
 
 <section class="flex flex-1 flex-col">
 	{@render children()}
 </section>
 
-<footer class="md:mx-30 mx-8 flex items-center justify-between py-10">
-	<div class="md:text-sm text-xs text-slate-400">
-		<img src="/author.webp" alt="Made by Retcy Logo" class="md:h-4 h-3" />
+<footer class="mx-8 flex items-center justify-between {page.url.pathname.startsWith("/itri/checkout") ? "pb-10" : "py-10"} md:mx-30">
+	<div class="text-xs text-slate-400 md:text-sm">
+		<img src="/author.webp" alt="Made by Retcy Logo" class="h-3 md:h-4" />
 		<p>&copy; {new Date().getFullYear()}, All rights reserved</p>
+		<p>
+			<a href="/docs/privacy" class="underline">Privacy Policy</a>
+			·
+			<a href="/docs/license" class="underline">License Agreement</a>
+		</p>
 	</div>
 
-	<img src="/morocco.webp" class="md:h-10 h-6" alt="Made in Morocco" />
+	<img src="/morocco.webp" class="h-6 md:h-10" alt="Made in Morocco" />
 </footer>
 
 <style>
@@ -93,7 +125,23 @@
 		}
 	}
 
+	@keyframes wave {
+		0% {
+			transform: rotate(0deg);
+		}
+		50% {
+			transform: rotate(-25deg);
+		}
+		100% {
+			transform: rotate(0deg);
+		}
+	}
+
 	.moving {
 		animation: move 4s linear infinite;
+	}
+
+	.waving {
+		animation: wave 500ms ease-in-out;
 	}
 </style>
