22af1bf9b0655c9f4d602c33d8582fada7ef621f
retr0cube <buthelo0@gmail.com>
2026-02-03 14:54:28 +0100

    feat: added a proper error page (yes really)

---
+++ apps/web/src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from "$app/state"
+</script>
+
+<div class="w-full flex-1 flex items-center justify-center flex-col">
+    <h1 class="font-bold text-4xl">
+      {page.status}  
+    </h1>
+    <p class="text-slate-600">{page.error?.message}</p>
+</div>
